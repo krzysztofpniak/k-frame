e0604ce457f9c8f304d1098fbe2ed3bdab3b187f
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-03-11 12:02:17 +0100

    feat(core/withDebug): new implementation that not uses debug from shallowEqual and accepts identifier

--- packages/core/src/withDebug.js
+++ packages/core/src/withDebug.js
@@ -1,12 +1,37 @@
-import shallowEqual from './shallowEqual';
 import React, {Component, createElement} from 'react';
+import {pick} from 'ramda';
+import shallowEqual, {is} from './shallowEqual';
 
-const withDebug = BaseComponent => {
-  const factory = createFactory(BaseComponent);
+const hasOwnProperty = Object.prototype.hasOwnProperty;
 
+const getChangedPropNames = (objA, objB) => {
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  const keys = keysA.length > keysB.length ? keysA : keysB;
+
+  const result = [];
+
+  for (let i = 0; i < keys.length; i++) {
+    if (!is(objA[keys[i]], objB[keys[i]])) {
+      result.push(keys[i]);
+    }
+  }
+
+  return result;
+};
+
+const withDebug = identifier => BaseComponent => {
   class WithDebug extends Component {
     componentDidUpdate(prevProps) {
-      shallowEqual(prevProps, this.props, true);
+      if (!shallowEqual(prevProps, this.props)) {
+        const changedProps = getChangedPropNames(prevProps, this.props);
+        console.log(
+          `changed ${identifier}: ${changedProps.join(', ')}`,
+          pick(changedProps, prevProps),
+          pick(changedProps, this.props)
+        );
+      }
     }
 
     render() {
