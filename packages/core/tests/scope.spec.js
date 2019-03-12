70b8f5db6baaf6f168c92e4e34e5a06963bacccb
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-03-12 12:22:57 +0100

    test(Scope): initial

---
+++ packages/core/tests/scope.spec.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Scope, KProvider} from '../src/main';
+import {render} from 'react-testing-library';
+import {createStoreMock} from './testData';
+
+beforeAll(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+});
+
+afterAll(() => {
+  console.error.mockRestore();
+});
+
+afterEach(() => {
+  console.error.mockClear();
+});
+
+describe('Scope', () => {
+  it('no logs when scope defined', () => {
+    const store = createStoreMock();
+
+    render(
+      <KProvider store={store}>
+        <Scope scope="someScope">
+          <div>some content</div>
+        </Scope>
+      </KProvider>
+    );
+
+    expect(console.error.mock.calls.length).toBe(0);
+  });
+
+  it('logs error when scope not defined', () => {
+    const store = createStoreMock();
+
+    render(
+      <KProvider store={store}>
+        <Scope>
+          <div>some content</div>
+        </Scope>
+      </KProvider>
+    );
+
+    expect(console.error.mock.calls.length).toBe(1);
+    expect(console.error.mock.calls[0][0]).toBe(
+      '<Scope> component requires scope param.'
+    );
+  });
+
+  it('logs error when used without KProvider', () => {
+    render(
+      <Scope scope="someScope">
+        <div>some content</div>
+      </Scope>
+    );
+
+    expect(console.error.mock.calls.length).toBe(1);
+    expect(console.error.mock.calls[0][0]).toBe(
+      '<Scope> requires <KProvider> at the top of the application.'
+    );
+  });
+});
