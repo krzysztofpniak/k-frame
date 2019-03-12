eb5a8783a872f55d930687f4d1c75ba0ee7c828b
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-03-12 12:23:34 +0100

    fix(Scope): adds missing errors (missing scope prop, missing KProvider)

--- packages/core/src/scope.js
+++ packages/core/src/scope.js
@@ -4,6 +4,19 @@ import {KContext} from './kLogicProvider';
 
 const Scope = ({scope, children}) => {
   const context = useContext(KContext);
+
+  if (!context.supplied) {
+    console.error(
+      '<Scope> requires <KProvider> at the top of the application.'
+    );
+    return null;
+  }
+
+  if (!scope) {
+    console.error('<Scope> component requires scope param.');
+    return null;
+  }
+
   const scopeArray = useMemo(() => scope.split('.'), [scope]);
   const newScope = useMemo(() => [...context.scope, ...scopeArray], [
     context.scope,
