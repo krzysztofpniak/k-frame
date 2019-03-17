006d68fc043d300227c535a71e68f4dc8aa0f8d3
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-03-17 21:49:40 +0100

    feat(sagas/useSagaRunner): cancel sagas on unmount

--- packages/sagas/src/useSagaRunner.js
+++ packages/sagas/src/useSagaRunner.js
@@ -1,14 +1,42 @@
-import {useCallback, useContext} from 'react';
+import {useCallback, useContext, useMemo, useEffect, useRef} from 'react';
 import {KContext} from '@k-frame/core';
+import {keys, map} from 'ramda';
 
-const useSagaRunner = () => {
+const useObjectValues = obj => {
+  const objKeys = useMemo(() => keys(obj), []);
+  const objValues = map(k => obj[k], objKeys);
+  return objValues;
+};
+
+const useSagaRunner = sagaContext => {
   const context = useContext(KContext);
+
+  const argsValues = useObjectValues(sagaContext);
+
+  const tasksRef = useRef([]);
+
+  useEffect(() => {
+    tasksRef.current = [];
+    return () => {
+      const tasks = tasksRef.current;
+      for (let i = 0; i < tasks.length; i++) {
+        tasks[i].cancel();
+      }
+      tasksRef.current = [];
+    };
+  }, []);
 
   const runSaga = useCallback(
     (saga, ...args) => {
-      context.runSaga(context.scope.join('.'), saga, ...args);
+      const task = context.runSaga(
+        {scope: context.scope, context: sagaContext},
+        saga,
+        ...args
+      );
+      tasksRef.current.push(task);
+      return task;
     },
-    [context.scope]
+    [context.scope, ...argsValues]
   );
 
   return runSaga;
