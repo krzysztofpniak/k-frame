78e63ef4935445c96409c72cf1603051ad7cfde2
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-04-02 18:33:21 +0200

    feat(forms/actions): adds setTouched action

--- packages/forms/src/actionTypes.js
+++ packages/forms/src/actionTypes.js
@@ -1,11 +1,7 @@
 const SET_FIELD = 'SetField';
+const SET_TOUCHED = 'SetTouched';
 const SUBMIT = 'Submit';
 const RESET = 'Reset';
 const SET_SUBMIT_DIRTY = 'SetSubmitDirty';
 
-export {
-    SET_FIELD,
-    SUBMIT,
-    RESET,
-    SET_SUBMIT_DIRTY,
-};
+export {SET_FIELD, SET_TOUCHED, SUBMIT, RESET, SET_SUBMIT_DIRTY};
--- packages/forms/src/actions.js
+++ packages/forms/src/actions.js
@@ -1,13 +1,21 @@
 import {createAction} from '@k-frame/core';
-import {SET_FIELD, SUBMIT, RESET, SET_SUBMIT_DIRTY} from './actionTypes';
+import {
+  SET_FIELD,
+  SET_TOUCHED,
+  SUBMIT,
+  RESET,
+  SET_SUBMIT_DIRTY,
+} from './actionTypes';
 
 const setField = createAction(SET_FIELD, (name, value, debounce) => ({
   name,
   value,
   debounce,
 }));
+
+const setTouched = createAction(SET_TOUCHED);
 const submit = createAction(SUBMIT);
 const reset = createAction(RESET);
 const setSubmitDirty = createAction(SET_SUBMIT_DIRTY);
 
-export {setField, submit, reset, setSubmitDirty};
+export {setField, setTouched, submit, reset, setSubmitDirty};
