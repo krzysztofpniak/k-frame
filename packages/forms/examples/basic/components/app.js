a7e71bb420a210db618e40dc574c6183736d9be6
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-04-02 15:05:53 +0200

    fix(examples/forms): remove prop with undefined value

--- packages/forms/examples/basic/components/app.js
+++ packages/forms/examples/basic/components/app.js
@@ -170,7 +170,6 @@ const App = () => {
         buttonsTemplate={Button}
         args={{color: colors[colorIndex]}}
         onSubmit={handleSubmit}
-        errorsDisplayStrategy={x}
       />
     </Scope>
   );
