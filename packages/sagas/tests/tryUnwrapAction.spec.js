7e9d3597d0e914dc25ce8c4f7400668d943723c3
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-04-08 14:41:03 +0200

    test(sagas/tryUnwrapAction): adds test with empty prefix

--- packages/sagas/tests/tryUnwrapAction.spec.js
+++ packages/sagas/tests/tryUnwrapAction.spec.js
@@ -1,9 +1,17 @@
 import tryUnwrapAction from '../src/tryUnwrapAction';
 
 describe('createScopeActionMatcher', () => {
-  it('should return false when no match', () => {
+  it('should return null when no match', () => {
     const unwrappedAction = tryUnwrapAction('prefix', {type: 'inc'});
     expect(unwrappedAction).toBe(null);
+  });
+
+  it('should pass action when prefix is empty', () => {
+    const action = {
+      type: 'inc',
+    };
+    const unwrappedAction = tryUnwrapAction('', action);
+    expect(unwrappedAction).toBe(action);
   });
 
   it('should return unwrapped action', () => {
