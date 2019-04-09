b6197a9631ba286d681d7174192fab09e161dcb0
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-04-09 19:18:20 +0200

    test(forms/formContext): initial

---
+++ packages/forms/tests/formContext.spec.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import FormContext from '../src/FormContext';
+import {render, cleanup} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+describe('FormContext', () => {
+  it('default throws errors', () => {
+    let context;
+    const ContextConsumer = () => {
+      context = useContext(FormContext);
+      return null;
+    };
+
+    render(<ContextConsumer />);
+
+    expect(() => context.getFieldState()).toThrow('FormContext not supplied');
+    expect(() => context.observable.subscribe()).toThrow(
+      'FormContext not supplied'
+    );
+    expect(() => context.mountField()).toThrow('FormContext not supplied');
+  });
+});
