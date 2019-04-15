5fdce270edd2a26d80472c9479dbcfc321f90345
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2019-04-15 12:30:21 +0200

    fix(forms/useFormStateObservable): call observer immediately only when it is already subscribed

--- packages/forms/src/useFormStateObservable.js
+++ packages/forms/src/useFormStateObservable.js
@@ -2,19 +2,7 @@ import {useEffect, useMemo, useRef} from 'react';
 import {Observable} from './micro-rx/index';
 
 const useFormStateObservable = (subscribe, getFormState) => {
-  const observer = useRef({});
-
-  useEffect(() => {
-    let prevState = getFormState();
-    observer.current.next(prevState);
-    return subscribe(() => {
-      const newState = getFormState();
-      if (newState !== prevState) {
-        observer.current.next(newState);
-        prevState = newState;
-      }
-    });
-  }, []);
+  const observer = useRef(null);
 
   const observable = useMemo(
     () =>
@@ -24,6 +12,20 @@ const useFormStateObservable = (subscribe, getFormState) => {
       }),
     []
   );
+
+  useEffect(() => {
+    let prevState = getFormState();
+    if (observer.current !== null) {
+      observer.current.next(prevState);
+    }
+    return subscribe(() => {
+      const newState = getFormState();
+      if (newState !== prevState) {
+        observer.current.next(newState);
+        prevState = newState;
+      }
+    });
+  }, []);
 
   return observable;
 };
