fd7616d7ccd0de7dff79f24cfa66320b2e70e062
Krzysztof Pniak <krzysztof.pniak@gmail.com>
2019-08-22 14:25:26 +0200

    fix(forms): adds support for null and false defaultValues

--- packages/forms/src/createFormReducer.js
+++ packages/forms/src/createFormReducer.js
@@ -45,7 +45,7 @@ const createUpdater = (fieldTypes, schema, resetOnSubmit, resetOnCancel) => {
   }
   const fields = compose(
     fromPairs,
-    map(f => [f.id, f.defaultValue || ''])
+    map(f => [f.id, f.defaultValue !== undefined ? f.defaultValue : ''])
   )(schema);
   const initialModel = getInitialModel(fields);
 
