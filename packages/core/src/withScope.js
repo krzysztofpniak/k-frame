9669990c890b01531252bbfb680f1168a77dd3c6
Krzysztof Pniak <krzysztof.pniak@cardinality.co.uk>
2021-04-23 13:49:25 +0200

    fix(core/withScope): fixes ref passing

--- packages/core/src/withScope.js
+++ packages/core/src/withScope.js
@@ -1,10 +1,11 @@
 import React, {forwardRef} from 'react';
+import {ForwardRef} from 'react-is';
 import Scope from './scope';
 
 const withScope = BaseComponent =>
   forwardRef((props, ref) => (
     <Scope scope={props.scope}>
-      <BaseComponent ref={ref} {...props} />
+      <BaseComponent ref={BaseComponent.$$typeof === ForwardRef ? ref : undefined} {...props} />
     </Scope>
   ));
 
