5b5971fc5593ca47b75f773e9eac4ce0c62aaf40
Krzysztof Pniak <krzysztof.pniak@gmail.com>
2022-08-17 14:14:39 +0200

    chore(release): changes to minor

--- release.sh
+++ release.sh
@@ -7,7 +7,8 @@ VERSION=""
 echo "-----REDUCERS-----"
 cd packages/core
 yarn build
-VERSION=$(npm version prerelease --preid=RC | cut -c2-)
+# VERSION=$(npm version prerelease --preid=RC | cut -c2-)
+VERSION=$(npm version minor | cut -c2-)
 npm publish
 
 echo "-----SAGAS-----"
