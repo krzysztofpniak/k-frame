de2ca923e7d31e6fea314d77bf86cf9c34c60e95
Krzysztof Pniak <krzysztof.pniak@gmail.com>
2023-07-07 12:38:57 +0200

    feat(forms): removes sagas from decorator

--- packages/forms/stories/helpers/kFrameDecorator.js
+++ packages/forms/stories/helpers/kFrameDecorator.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {compose, identity} from 'ramda';
-import {applyMiddleware, createStore} from 'redux';
-import {scopedSagaMiddleware} from '@k-frame/sagas';
+import {createStore} from 'redux';
 import {KProvider} from '@k-frame/core';
 
 const storeFactory = compose(
-  applyMiddleware(scopedSagaMiddleware),
   window.__REDUX_DEVTOOLS_EXTENSION__
     ? window.__REDUX_DEVTOOLS_EXTENSION__()
     : identity
@@ -14,9 +12,7 @@ const storeFactory = compose(
 const emptyStore = storeFactory(a => a, {});
 
 const kFrameDecorator = story => (
-  <KProvider store={emptyStore} runSaga={scopedSagaMiddleware.run}>
-    {story()}
-  </KProvider>
+  <KProvider store={emptyStore}>{story()}</KProvider>
 );
 
 export default kFrameDecorator;
